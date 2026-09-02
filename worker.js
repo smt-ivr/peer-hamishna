c02ca9d9377dd77a5c035c6d86efc385c6638d66
smt-ivr <s6754091@gmail.com>
2026-09-02 12:16:56 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,6 +2,8 @@ import indexHtml from './index.html';
 import stylesCss from './styles.css';
 import clientAppJs from './client-app.js';
 import clientExamUpdateJs from './client-exam-update.js';
+import clientStudentsJs from './client-students.js';
+import clientExamsJs from './client-exams.js';
 
 export default {
   async fetch(request, env, ctx) {
@@ -19,6 +21,12 @@ export default {
     }
     if (path === '/peer/client-exam-update.js') {
       return new Response(clientExamUpdateJs, { status: 200, headers: { 'Content-Type': 'application/javascript; charset=utf-8' } });
+    }
+    if (path === '/peer/client-students.js') {
+      return new Response(clientStudentsJs, { status: 200, headers: { 'Content-Type': 'application/javascript; charset=utf-8' } });
+    }
+    if (path === '/peer/client-exams.js') {
+      return new Response(clientExamsJs, { status: 200, headers: { 'Content-Type': 'application/javascript; charset=utf-8' } });
     }
 
     return new Response('404 - Not Found', { status: 404, headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
